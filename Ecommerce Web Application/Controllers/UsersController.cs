d285693f6d44d7c26db2787f48ca1c0c6a30e6b5
ardx3000 <daniel.ardx@gmail.com>
2024-01-20 06:03:04 +0000

    User Profile
    Added the ability for the user to edit info

--- Ecommerce Web Application/Controllers/UsersController.cs
+++ Ecommerce Web Application/Controllers/UsersController.cs
@@ -44,7 +44,7 @@ namespace Ecommerce_Web_Application.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Welcome");
+                    return RedirectToAction("UserProfile");
                 }
 
                 foreach (var error in result.Errors)
@@ -68,11 +68,72 @@ namespace Ecommerce_Web_Application.Controllers
 
             if (result.Succeeded)
             {
-                return RedirectToAction("Welcome");
+                return RedirectToAction("UserProfile");
             }
 
             ModelState.AddModelError("", "Invalid login attempt! ");
             return View();
         }
+
+        public async Task<IActionResult> UserProfile()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                return View(user);
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UserProfile(UserViewModel updateUser)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                user.Email = updateUser.Email;
+                user.PhoneNumber = updateUser.PhoneNumber;
+
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("UserProfile");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(updateUser);
+        }
+
+        public IActionResult ProfileUpdated()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Logut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
